fe3533f870f5d74eeaa329bf484122e3de64bb95
JustBoyke <mail@boykevanvugt.nl>
2021-11-17 22:00:25 +0100

    Updated with correct code

--- src/me/boykev/totem/Main.java
+++ src/me/boykev/totem/Main.java
@@ -8,9 +8,7 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
@@ -48,28 +46,5 @@ public class Main extends JavaPlugin implements Listener{
 		}
 	}
 	
-//	@EventHandler
-//	public void onDeath(EntityDamageByEntityEvent e) {
-//		if(!(e.getEntityType() == EntityType.PLAYER)) {
-//			return;
-//		}
-//		Player p = (Player) e.getEntity();
-//		if(p.getName().equalsIgnoreCase("boykev")) {
-//			if(p.getHealth() - e.getDamage() < 1) {
-//				if(p.getInventory().contains(new ItemStack(Material.TOTEM_OF_UNDYING))) {
-//					e.setCancelled(true);
-//					p.playEffect(EntityEffect.TOTEM_RESURRECT);
-//					p.setHealth(20);
-//					p.getWorld().playEffect(p.getLocation(), Effect.DRAGON_BREATH, 10);
-//					p.sendMessage(ChatColor.RED + "Een TOTEM heeft je leven gered!");
-//					return;
-//				}
-//				e.setCancelled(true);
-//				p.playEffect(EntityEffect.TOTEM_RESURRECT);
-//				p.setHealth(20);
-//				p.getWorld().playEffect(p.getLocation(), Effect.DRAGON_BREATH, 10);
-//			}
-//		}
-//	}
 
 }
